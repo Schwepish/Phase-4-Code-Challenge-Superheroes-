59eb65d205b99e086ad578bf6ecd5f8a1a66cbf9
schwepish <felix.kahuta@student.moringaschool.com>
2024-01-16 09:47:42 +0300

    Finished

---
+++ moringa/phase-3/sQl/RIP.session.sql
@@ -0,0 +1,5 @@
+CREATE TABLE  students (
+    id SERIAL PRIMARY KEY,
+    name varchar (250),
+    email varchar (500),
+    phone INTEGER)
---
+++ moringa/phase-3/sQl/SQL/Postgresql/Table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE  students (
+    id INTEGER PRIMARY KEY,
+    name varchar (250),
+    email varchar (500),
+    phone INTEGER)
